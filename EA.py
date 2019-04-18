9f0dacf74792922038277cfee4ecd37cddfa19f3
Jared Clark <clarkr28@msu.edu>
2019-04-18 03:10:11 -0400

    created new binary individual where the genome is completely binary, uploaded logs from runs

--- EA.py
+++ EA.py
@@ -1,12 +1,13 @@
-# purpose of this file is to run an EA to evolve an IDS for a can netowrk
+# purpose of this file is to run an EA to evolve an IDS for a can network
+# written by Jared Clark for CSE 914
 
 # notes:
 #   data labels: 'R' means normal, 'T' means injected
-#   genome: 2 bytes of IDs, 8 bytes of data
 
 import random
 import pickle # CAN data was saved in pkl format
 from ea_individual import Individual
+from ea_individual_binary import IndividualBinary
 from datetime import datetime
 import os
 
@@ -27,13 +28,12 @@ def logger(fname, pop, avg_fitness, max_fitness, min_fitness, generation):
     log_data['min_fitness'] = min_fitness
     log_data['individuals'] = [p.serialize() for p in pop]
     log_data['generation'] = generation
-    f = open(fname)
-    pickle.load(log_data, f)
+    f = open(fname, 'wb')
+    pickle.dump(log_data, f)
     f.close()
 
 
-
-def run(pop_size, test_size, num_gens, log_freq, can_data_fname, mut_prob):
+def run(pop_size, test_size, num_gens, log_freq, can_data_fname, mut_prob, keep_num):
     """
     run the EA with the specified parameters
     :param pop_size: population size to use
@@ -56,14 +56,15 @@ def run(pop_size, test_size, num_gens, log_freq, can_data_fname, mut_prob):
     run_params['log_freq'] = log_freq
     run_params['can_data_fname'] = can_data_fname
     run_params['mut_prob'] = mut_prob
-    f = open(run_dir + RUN_PARAMS_FNAME)
+    run_params['keep_num'] = keep_num
+    f = open(run_dir + RUN_PARAMS_FNAME, 'wb')
     pickle.dump(run_params, f)
     f.close()
 
     # generate an initial population
     pop = []
     for i in range(pop_size):
-        pop.append(Individual())
+        pop.append(IndividualBinary())
 
     # load the testing data
     f = open(CAN_DATA_DIR + can_data_fname, 'rb')
@@ -77,7 +78,7 @@ def run(pop_size, test_size, num_gens, log_freq, can_data_fname, mut_prob):
         # get subset of data to test with (all the test data would be too slow)
         test_inds = []
         while len(test_inds) < test_size:
-            ind = random.randint(0, len(can_data))
+            ind = random.randint(0, len(can_data) - 1)
             if ind not in test_inds:
                 test_inds.append(ind)
         test_data_subset = [can_data[i] for i in test_inds]
@@ -91,6 +92,22 @@ def run(pop_size, test_size, num_gens, log_freq, can_data_fname, mut_prob):
         avg_fitness = sum(fitnesses) / len(pop)
         print('gen {} fitnesses - max: {}, avg: {}'.format(i, max_fitness, avg_fitness))
 
+        # get the n best individuals
+        # start with the first n and sort them
+        n_best = pop[0:keep_num]
+        n_best.sort(key=lambda x: x.get_fitness(), reverse=True)
+        # filter through the rest and swap up
+        for i, p in enumerate(pop[keep_num:]):
+            if p.get_fitness() > n_best[-1].get_fitness():
+                n_best[-1] = p
+                index = len(n_best) - 2
+                while index >= 0 and n_best[index].get_fitness() < n_best[index+1].get_fitness():
+                    temp = n_best[index]
+                    n_best[index] = n_best[index+1]
+                    n_best[index+1] = temp
+                    index -= 1
+
+
         """
         log data
         """
@@ -99,13 +116,13 @@ def run(pop_size, test_size, num_gens, log_freq, can_data_fname, mut_prob):
                    pop, avg_fitness, max_fitness, min(fitnesses), i)
 
         # select individuals for next generation
-        next_gen = []
+        next_gen = n_best
         for p in pop:
             if random.random() < p.get_fitness() / max_fitness:
                 next_gen.append(p)
 
         # crossover
-        for j in range(0, len(next_gen), 2):
+        for j in range(keep_num, len(next_gen), 2):
             if j+1 < len(next_gen):
                 next_gen[j].cross(next_gen[j+1])
 
---
+++ ea_individual_binary.py
@@ -0,0 +1,78 @@
+# definition of the individual for the EA
+
+import random
+
+# parameters about the genome/CAN specific parameters
+ID_RANGE = 2**16
+BYTE_SIZE = 2**8
+SER_TESTED = 'tested'
+SER_FITNESS = 'fitness'
+SER_GENOME = 'genome'
+
+
+class IndividualBinary:
+    def __init__(self):
+        self.tested = False  # signify that the individual has not been tested yet
+        self.fitness = 0
+        self.genome = []
+        for i in range(8 * 10):  # 8 bits per byte * 10 bytes in a CAN message (ID + DATA field)
+            # first binary value indicates if the bit is used
+            # second binary value indicates what value to look for
+            self.genome.append([random.random() > 0.5, random.random() > 0.5])
+
+    def test(self, test_data):
+        total_score = 0
+        for sample in test_data:
+            prediction = 0
+            binary_string = '{0:016b}'.format(sample[0])
+            for i in range(sample[1]):
+                binary_string += '{0:08b}'.format(sample[i+2])
+            for i, val in enumerate([x == '1' for x in binary_string]):
+                if self.genome[i][0] and self.genome[i][1] == val:
+                    prediction += 1
+
+            if sample[-1] == 'T':
+                total_score += prediction
+            else:
+                total_score -= prediction / 10
+
+        self.tested = True
+        self.fitness = total_score
+        return self.fitness
+
+    def get_fitness(self):
+        if self.tested:
+            return self.fitness
+        else:
+            raise Exception("get_fitness was called when there was not a valid fitness value")
+
+    def mutate(self, prob):
+        for i in range(len(self.genome)):
+            if random.random() < prob:
+                self.genome[i][0] = not self.genome[i][0]
+                self.tested = False
+            if random.random() < prob:
+                self.genome[i][0] = not self.genome[i][0]
+                self.tested = False
+        return None
+
+    def cross(self, other):
+        cross_point = random.randint(1,len(self.genome) - 1)
+        temp = self.genome[cross_point:]
+        self.genome[cross_point:] = other.genome[cross_point:]
+        other.genome[cross_point:] = temp
+        self.tested = False
+        return None
+
+    def serialize(self):
+        data = {}
+        data[SER_TESTED] = self.tested
+        data[SER_FITNESS] = self.fitness
+        data[SER_GENOME] = self.genome
+        return data
+
+    def load(self, data):
+        self.genome = data[SER_GENOME]
+        self.fitness = data[SER_FITNESS]
+        self.tested = data[SER_TESTED]
+        return None
