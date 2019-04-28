efa34d2b57515de8c69124ec00e381b834c66ab8
Jared Clark <clarkr28@msu.edu>
2019-04-28 12:48:35 -0400

    deleted unnessary logs and ran more Fuzzy tests with batch9.  These results should be the results used in the paper

--- batch9.py
+++ batch9.py
@@ -6,10 +6,13 @@ from EA import run
 
 # run: pop_size, test_size, num_gens, log_freq, can_data_fname, mut_prob, keep_num
 
-run_title = 'Fuzzy-3k-keep3-parallel'
+run_title_base = 'Fuzzy-3k-keep3-mut'
+muts = [0.00625, 0.0125, 0.025, 0.05, 0.075, 0.1, 0.15]
 
-try:
-    run(200, 1000, 30, 50, 'Fuzzy_h_dataset.pkl', 0.0125, 3, run_title)
-except Exception as e:
-    print(e)
+for m in muts:
+    run_title = run_title_base + str(m).split('.')[1]
+    try:
+        run(500, 500, 1000, 100, 'Fuzzy_train.pkl', m, 3, run_title)
+    except Exception as e:
+        print(e)
 
--- logs/2019-04-18-23-20-16/README.md
+++
@@ -1,3 +0,0 @@
-This run used the entire fuzzy dataset and tested every individual with 5000 CAN
-messages, which seemed to work better than the previous test that only tested
-every individual with 500 CAN messages.  
